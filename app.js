358645edbc4cc815d440faf57eb3dc3fbc44d3ac
StaxGit <lario.free@gmail.com>
2020-04-27 18:29:55 +0200

    Setup mongoose db connection

--- app.js
+++ app.js
@@ -4,19 +4,30 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const expect = require("chai").expect;
-const dotenv = require("dotenv").config
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
 const apiRoutes = require("./routes/api.js");
 const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner.js");
 
 const cors = require("cors");
-app.use(cors({origin: "*"})) //For FCC testing
+app.use(cors({ origin: "*" })) //For FCC testing
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose connected to the database!");
+})
 
 //Sample front-end
 app.route('/b/:board/')
@@ -42,9 +53,9 @@ apiRoutes(app);
 
 //Sample Front-end
 
-    
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -54,15 +65,15 @@ let port = process.env.PORT || 8000;
 //Start our server and tests!
 app.listen(port, function () {
   console.log(`Listening on port ${port}!`);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log('Tests are not valid:');
+        console.log(error);
       }
     }, 1500);
   }
